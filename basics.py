423e5c9fb8bd52164ac846810d3654580133d3fd
DiptikaD <diptikadevi@gmail.com>
2024-12-05 12:12:16 -0500

    operators

--- basics.py
+++ basics.py
@@ -6,3 +6,15 @@ amount = "three"
 #can set a type hint for the user to know what the type should be, however python will ignore it. using type checkers like mypy will flag it as an error
 amount : int = 3
 amount = "three"
+
+#integer division operator returns how many times one whole number fully divides into another
+26//7
+# output = 3. this is because 7 > 14 > 21 > 28
+# it can divide up to 21 with remainder, but it took 3 times div to get to 26.
+
+26%7
+# output = 5. it is the remainder left over after dividing the full numbers.
+
+#operator for exponential operations 
+2**10
+# output = 1024. this is 2^10
