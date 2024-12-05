0967f8e88568e2e3f6c0b011002821b0e47063e1
DiptikaD <diptikadevi@gmail.com>
2024-12-05 11:27:03 -0500

    variable typing

--- basics.py
+++ basics.py
@@ -1,2 +1,8 @@
+
+#variables in python are not strongly typed. variables will update the value based on the most current assignment
 amount = 3
+amount = "three"
+
+#can set a type hint for the user to know what the type should be, however python will ignore it. using type checkers like mypy will flag it as an error
+amount : int = 3
 amount = "three"
