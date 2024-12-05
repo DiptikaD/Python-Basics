a03678111a046ed5f769c235942746c7b27869cf
DiptikaD <diptikadevi@gmail.com>
2024-12-05 14:04:21 -0500

    fstring exercise and formatting

--- basics.py
+++ basics.py
@@ -30,3 +30,26 @@ print(message.islower())
 print(message + "!")
 print(message.replace("weird", "stinky"))
 #keep quotes consistent "" '' and use escape characters if needed \n \' \t
+
+# read tracebacks from bottom upward for most oldest at the bottom of the message, and newest at top.
+
+# print() does not have a return value, it is None (py versions of void)
+
+# to run py on cmd, navigate to the dir containing the file. then 
+#python3 basics.py
+#it will run all the code and print output
+
+#can easily parse 
+four = 4
+fourStr = str(four)
+print(fourStr)
+fourInt = int(fourStr)
+print(fourInt)
+
+## input and output is easy
+name = input("Who is this?? ")
+print("oh its " + name + "!")
+
+#could have also written as
+print(f"oh its {name}!")
+#this is a fstring, py looks for the code within {} and will convert the variable or code to fit into the fstring. it saves having to put +s and str(5) to convert ints into str.
