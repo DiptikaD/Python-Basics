370435d3ee6c5eb399b238fff2ef48c19931bf2f
DiptikaD <diptikadevi@gmail.com>
2024-12-05 12:23:04 -0500

    sqaurex exercise

---
+++ squarex/square.py
@@ -0,0 +1,13 @@
+# Problem Statement
+# For this exercise, I'd like you to make two variables: x and y.
+
+# The x variable should contain your favorite number (or any number at all).
+
+# The y variable should contain the square of x.
+
+# Don't write a number directly when assigning y though! Instead, use an appropriate arithmetic operator to square x instead.
+
+x = 9
+y = x*x
+
+#ran test_square and got all ok!
