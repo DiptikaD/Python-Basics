20806f55d90a6ac8a14753e9f877d2de0a71b307
DiptikaD <diptikadevi@gmail.com>
2024-12-10 15:56:50 -0500

    unsmart quotes exercise

---
+++ unsmartquotes/unsmart.py
@@ -0,0 +1,28 @@
+# Problem Statement
+# I'd like you to write a program called unsmart.py which accepts a file as an argument and prints out the contents of the file with smart quotes converted to "dumb" quotes. Smart quotes are the angled quotes and apostrophes, namely ‘, ’, “, and ”.
+
+# In many word processors (MS Word, Google Drive, etc.) typing in the ' character on a US-style keyboard will result in the word processor entering either ‘ or ’ and typing in " will result in either “, or ”.
+
+# basically replace “ with ", ect
+
+#provided code:
+
+from argparse import ArgumentParser, FileType
+
+parser = ArgumentParser()
+parser.add_argument("file", type=FileType(encoding="utf-8"))
+args = parser.parse_args()
+
+text = args.file.read()
+
+# TODO unsmartify the text somehow
+
+print(text
+      .replace('“', '"')
+      .replace('”', '"')
+      .replace('‘', '\'')
+      .replace('’', '\'')
+      .rstrip("\n")
+      )
+
+#you can line up functions on the same variable to get a big chunk done
