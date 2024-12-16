84c4d2e93a90b3d419e77ab989440068e972dadd
DiptikaD <diptikadevi@gmail.com>
2024-12-16 12:37:16 -0500

    added notes and readability

--- observe/observe.py
+++ observe/observe.py
@@ -9,10 +9,15 @@ commands = ["Listen", "Look", "Smell", "Sit"]
 
 
 for command in commands:
+        ##flush = true means that the command will buffer (stay on screen until CLEAR) 
+        ## end="" is the character that comes after the addition of the ".", this is important as without specifying, it will default a new line!
     print(command, end="", flush=True)
     sleep(1)
+
     for i in range(0,3):
         print(".", end="", flush=True)
         sleep(1)
+
     print(CLEAR, end="")
+    
 print("Done")
