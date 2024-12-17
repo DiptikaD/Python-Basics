f0ce0a220ff717851e0f53d9a01c7fa70fac278c
DiptikaD <diptikadevi@gmail.com>
2024-12-17 13:05:44 -0500

    jot exercise initial commit

--- files.py
+++ files.py
@@ -5,7 +5,16 @@
 
     ## reading line by line can be done via enumerate.
 
-filename = "diary980.md"
-with open(filename) as diary_file:
-    for n, line in enumerate(diary_file, start=1):  ## for each line, it will count up from 1
-        print(n, line.rstrip("\n"))   ## print/read with lines numbered, removing extra \n
+# filename = "diary980.md"
+# with open(filename) as diary_file:
+#     for n, line in enumerate(diary_file, start=1):  ## for each line, it will count up from 1
+#         print(n, line.rstrip("\n"))   ## print/read with lines numbered, removing extra \n
+
+
+    ## writing files
+with open("my_file.txt", mode="wt") as file:    ## as there is no file called my_file.txt, it creates it. the mode="wt" means that the mode which is default to "r" for "read-only", is instead "wt", for write text.
+    file.write("here is text\n")
+    file.write("here is more\n")
+
+
+    
