7ef089d1b3c7230334d76c1ce6dc8a087494a713
DiptikaD <diptikadevi@gmail.com>
2024-12-24 11:39:03 -0500

    joining looper helper notes

--- looping_helpers.py
+++ looping_helpers.py
@@ -15,3 +15,19 @@ for colour in reversed(colours):
 iterateRev = reversed(colours)
 print(next(iterateRev), next(iterateRev), next(iterateRev))
     ## "don't store their results anywhere. Instead, these functions return objects that are meant to be looped over immediately".
+
+#   JOINING
+    #universally (for all datastructures + files + numbers) can join together using .join()
+joined = " ".join(colours)
+print(joined)
+    ## the " " is a delimiter, it will be inserted between each entry, it can be anything, it can also be nothing
+joined2 = " ugh! ".join(colours)
+print(joined2)
+file=open("my_file.txt")
+filejoin = ", ".join(line.rstrip("\n") for line in file)    ## this removes the \n and replaces with , 
+print(filejoin)
+
+    ## with numbers it must be converted to str
+numbers = [3,4,4,3,5,5,66,7,7]
+print(" $".join(str(n) for n in numbers))
+    ## can do an inline for loop to convert to str and join.
