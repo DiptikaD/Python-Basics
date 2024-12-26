97a7bd97b894ecf0a328cdd5ad9c9d0384b883a2
DiptikaD <diptikadevi@gmail.com>
2024-12-26 10:14:41 -0500

    solved all bonus

--- grades/grades.py
+++ grades/grades.py
@@ -20,3 +20,13 @@ def percent_to_grade(input_grade,*, round=False, suffix=False): # keyword only *
             print(grade, input_grade, round_grade)
             return grade
 
+      
+def calculate_gpa(list_of_scores):
+    points = {"A+":4.33, "A":4.00, "A-":3.67, "B+":3.33, "B":3.00, "B-":2.67, "C+":2.33, "C":2.00, "C-":1.67, "D+":1.33, "D":1.00, "D-":0.67, "F":0.00}
+    sum = 0
+
+    for score in list_of_scores:
+        for key, value in points.items():
+            if score == key:
+                sum += value
+    return sum / len(list_of_scores)
