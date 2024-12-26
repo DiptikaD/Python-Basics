afb8021a273d58a70c155e44987e3b5a5507bde2
DiptikaD <diptikadevi@gmail.com>
2024-12-26 12:43:11 -0500

    implicit line continuation notes

--- dictionaries.py
+++ dictionaries.py
@@ -29,3 +29,19 @@ print(scores,slo)
     # could attempt to delete a key that doesnt exist by dodging a keyerr
 scores.pop("sti", "key not found")  # without the second statement, a keyerr would be given
 scores.pop("hof", False)
+
+#   implicit line continuation
+    # where lines can be broken up without whitespace mattering when the code block is within ()[]{}
+print("a"                 + 
+              "b", 
+              
+              scores["pig"]) 
+    # this is best done for formatting for readability, but could also just create a mess for no good reason
+    # can also use \to wrap
+import math, mailbox, \
+        cmath, time # without the \ python will get upset
+    # could also simple wrap the imports with ()
+from collections.abc import (
+    Hashable,Iterable, KeysView, Mapping,
+        MutableMapping, Set)
+    # functions and data structures already have their own brackets which you can format with
