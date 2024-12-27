80c79aea4a5cbc68ae5eb8010d461e93291ea9bb
DiptikaD <diptikadevi@gmail.com>
2024-12-27 14:52:16 -0500

    exercise solved

--- length or none/len_or_none.py
+++ length or none/len_or_none.py
@@ -1,1 +1,8 @@
-    # initial commit
+def len_or_none(input):
+    try:    
+        return len(input)
+    except TypeError:   # error that pops up when an int is passed through
+        return None     # exception for the int err
+    
+    # all tests pass
+    # note that exceptions dont have to return error messages, they can just be for exceptions to a normal case
