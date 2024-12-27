3f3528aef107f2eeb102321cfde3de3e5e56d880
DiptikaD <diptikadevi@gmail.com>
2024-12-27 12:32:24 -0500

    other solution, bonus 1

--- flip_dict2/flip.py
+++ flip_dict2/flip.py
@@ -7,3 +7,16 @@ def flip_dict_of_lists(dict_of_lists):
 ## base solution
     # was difficult due to the very intricate wording of certain functions (.append).
     # did not succeed in bonuses
+
+## other solutions
+## BONUS 1
+from collections import defaultdict
+
+def flip_dict_of_lists(dict_of_lists, dict_type=None):
+    """Return a "flipped" dictionary of lists."""
+    flipped = defaultdict(list) # defaultdict creates mapping with default for all key lookups
+    for old_key, old_values in dict_of_lists.items():
+        for value in old_values:
+            flipped[value].append(old_key)
+    return flipped if dict_type is None else dict_type(flipped) # if dict_type was specified, parse it through and return
+
