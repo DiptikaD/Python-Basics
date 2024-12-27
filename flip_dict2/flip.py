0fe67c910014b00c0ee45d01e3a5481198dff9d4
DiptikaD <diptikadevi@gmail.com>
2024-12-27 12:31:51 -0500

    base solution

--- flip_dict2/flip.py
+++ flip_dict2/flip.py
@@ -1,1 +1,9 @@
-    #initial commit
+def flip_dict_of_lists(dict_of_lists): 
+    flipped = {}
+    for key, values in dict_of_lists.items():
+        for value in values:     # dict_of_lists={key:[value, value], key:[value]}
+            flipped.setdefault(value, []).append(key)
+    return flipped
+## base solution
+    # was difficult due to the very intricate wording of certain functions (.append).
+    # did not succeed in bonuses
