a9d748ea21452eb76d9fe5af0dc169eea5a2c32b
DiptikaD <diptikadevi@gmail.com>
2024-12-30 14:09:06 -0500

    solved exercise!

--- negate/negate.py
+++ negate/negate.py
@@ -1,1 +1,14 @@
-    # initial commit
+def negate(list_of_lists):  #[[1, -2], [-3,4]]
+
+    return [
+        [-each_value for each_value in each_index]  # inner for
+        for each_index in list_of_lists     # outer
+    ]
+
+    negated = []
+    for each_index in list_of_lists:    # outer
+        negative_values = []
+        for each_value in each_index:       # inner
+            negative_values.append(-each_value)
+        negated.append(negative_values)
+    return negated
