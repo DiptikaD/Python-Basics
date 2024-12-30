6b9bb3fbd7793bbb489c7480737b8ff2058fff86
DiptikaD <diptikadevi@gmail.com>
2024-12-30 14:20:06 -0500

    exercise solved!

--- negate/negate.py
+++ negate/negate.py
@@ -1,3 +1,13 @@
+## solved! i understand the formatting of the list comprehension a bit better now. the inner for loop sits atop in its own [], then outside of that loop is the top layer of the logic
+## therefore the anatomy is 
+    # return [
+    #   [inner for loop]
+    #   outer for loop
+    # ]
+
+## i apprached the problem by writing out the full double forloops, then attempted to format the list comprehension by inserting information as needed.
+
+
 def negate(list_of_lists):  #[[1, -2], [-3,4]]
 
     return [
@@ -12,3 +22,8 @@ def negate(list_of_lists):  #[[1, -2], [-3,4]]
             negative_values.append(-each_value)
         negated.append(negative_values)
     return negated
+
+# solved all tests!
+
+## other solutions
+    ## look identical to mine!!
