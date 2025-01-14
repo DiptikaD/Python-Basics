c5f53b8d86e380b48f931b45374d1f96cc8f6b4f
DiptikaD <diptikadevi@gmail.com>
2025-01-14 09:57:23 -0500

    join word

--- exercism_exercises/little_sisters_vocab/vocab.py
+++ exercism_exercises/little_sisters_vocab/vocab.py
@@ -2,3 +2,8 @@
 
 def add_prefix_un(word):
     return f"un{word}"
+
+
+def make_word_groups(vocab_words):
+    return " :: ".join([vocab_words[0]] + [vocab_words[0] + word for word in vocab_words[1:]])
+
