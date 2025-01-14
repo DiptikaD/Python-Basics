3bea8932f492328bda9c1b27f87d20c92080733a
DiptikaD <diptikadevi@gmail.com>
2025-01-14 09:48:13 -0500

    concat, contains, avg

--- exercism_exercises/card_games/card_games.py
+++ exercism_exercises/card_games/card_games.py
@@ -6,3 +6,15 @@ Python list documentation: https://docs.python.org/3/tutorial/datastructures.htm
 
 def get_rounds(number):
     return [number, number+1, number+2]
+
+def concatenate_rounds(rounds_1, rounds_2):
+    return rounds_1 + rounds_2
+
+def list_contains_round(rounds, number):
+    return number in rounds
+
+def card_average(hand):
+    product = 0
+    for card in hand:
+        product += card
+    return product/len(hand)
