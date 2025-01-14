4969585df8ef784df74fabc5e2b63269bf5c13b9
DiptikaD <diptikadevi@gmail.com>
2025-01-14 09:47:17 -0500

    get rounds

--- exercism_exercises/card_games/card_games.py
+++ exercism_exercises/card_games/card_games.py
@@ -1,1 +1,8 @@
-    ## initial commit
+"""Functions for tracking poker hands and assorted card tasks.
+
+Python list documentation: https://docs.python.org/3/tutorial/datastructures.html
+"""
+
+
+def get_rounds(number):
+    return [number, number+1, number+2]
