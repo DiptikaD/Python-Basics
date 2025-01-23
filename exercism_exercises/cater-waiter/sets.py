afc8cc76a3ecca5dfb19da85063f4900f55e90a7
DiptikaD <diptikadevi@gmail.com>
2025-01-23 13:32:37 -0500

    check drinks

--- exercism_exercises/cater-waiter/sets.py
+++ exercism_exercises/cater-waiter/sets.py
@@ -8,3 +8,7 @@ from sets_categories_data import (VEGAN,
 
 def clean_ingredients(dish_name, dish_ingredients):
     return dish_name, set(dish_ingredients)
+
+def check_drinks(drink_name, drink_ingredients):
+    return f"{drink_name} Mocktail" if set(drink_ingredients).isdisjoint(
+        set(ALCOHOLS)) else f"{drink_name} Cocktail"
