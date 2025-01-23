824a6c1e7dc8e188875f1959bcf24c19977eeeed
DiptikaD <diptikadevi@gmail.com>
2025-01-23 13:56:56 -0500

    categorise dish

--- exercism_exercises/cater-waiter/sets.py
+++ exercism_exercises/cater-waiter/sets.py
@@ -12,3 +12,11 @@ def clean_ingredients(dish_name, dish_ingredients):
 def check_drinks(drink_name, drink_ingredients):
     return f"{drink_name} Mocktail" if set(drink_ingredients).isdisjoint(
         set(ALCOHOLS)) else f"{drink_name} Cocktail"
+
+def categorize_dish(dish_name, dish_ingredients):
+    categories = [("VEGAN", VEGAN), ("VEGETARIAN", VEGETARIAN), 
+                  ("PALEO", PALEO), ("KETO", KETO), ("OMNIVORE", OMNIVORE)]
+    
+    for category_name, category_ingredients in categories:
+        if dish_ingredients.issubset(category_ingredients) == True:
+            return f"{dish_name}: {category_name}"
