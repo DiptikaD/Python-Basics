6aa25304056db5bc5aa19a50bd7f4d18fff3cea7
DiptikaD <diptikadevi@gmail.com>
2025-01-24 11:12:12 -0500

    deep unpacking + comma

--- unpacking.py
+++ unpacking.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 ## unpacking can be done as long as items on both sides of the equation are equal
 x,y = (4,5)
 # x,y,z = (4,5) this is unbalanced and will throw an error
@@ -26,3 +28,20 @@ colour, point = ("green", (1,2,3))
 print(colour, point, "deep unpacking")
     # although it is assuming you know how many values is within those 
     # data structures
+
+## can deep unpack an iterable
+items = [4,2,5,6,2,4]
+for i, (first, last) in enumerate(zip(items, reversed(items))):
+    if first != last:
+        raise ValueError(f"item {i} does not match: {first} != {last}")
+    
+# another example
+def most_common(items):
+    (value, times_seen), = Counter(items).most_common(1)
+    return value
+## in this example there is a , which plays a strong part in unpacking
+# without the , it would result in an error
+# the comma allows for the first output from Counter to be unpacked
+# and that first index of that first output being:
+    # value
+    # times_seen
