77734ef54990306e8cc3b13287eb266f7b0d02f7
DiptikaD <diptikadevi@gmail.com>
2025-01-24 15:36:54 -0500

    add missing stops

--- exercism_exercises/locomotive_engineer/locomotive_engineer.py
+++ exercism_exercises/locomotive_engineer/locomotive_engineer.py
@@ -7,3 +7,10 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     *combined, = third, *missing_wagons, *rest, first, second 
     # NOTE FOR ABOVE: the items with the * are the because they are a list/tuple, not just one value
     return combined
+
+def add_missing_stops(route, **kwargs):
+
+    stops = list(kwargs.values())
+    route["stops"] = stops
+    
+    return route
