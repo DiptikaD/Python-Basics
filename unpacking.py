2b5a510f4b1060a34ca5ae4165a838bd2072daeb
DiptikaD <diptikadevi@gmail.com>
2025-01-24 10:21:40 -0500

    unpacking

---
+++ unpacking.py
@@ -0,0 +1,7 @@
+## unpacking can be done as long as items on both sides of the equation are equal
+x,y = (4,5)
+# x,y,z = (4,5) this is unbalanced and will throw an error
+numbers = [1,2,3,4,5]
+one, two, three, four, five = numbers
+print(three)
+
