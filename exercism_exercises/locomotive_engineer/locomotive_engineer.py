0abfa4ea95690198a085272b080da9126e5c9d56
DiptikaD <diptikadevi@gmail.com>
2025-01-24 15:36:21 -0500

    fix list of wagons

--- exercism_exercises/locomotive_engineer/locomotive_engineer.py
+++ exercism_exercises/locomotive_engineer/locomotive_engineer.py
@@ -1,3 +1,9 @@
 def get_list_of_wagons(*args):
     *values, = args
     return values
+
+def fix_list_of_wagons(each_wagons_id, missing_wagons):
+    first, second, third, *rest = each_wagons_id
+    *combined, = third, *missing_wagons, *rest, first, second 
+    # NOTE FOR ABOVE: the items with the * are the because they are a list/tuple, not just one value
+    return combined
